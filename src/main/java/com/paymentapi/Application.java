785b4c4cc2b704ee18f58abd52e995183b056325
Paul Albu <paul.i.albu@gmail.com>
2024-06-05 16:12:08 +0300

    test

--- src/main/java/com/paymentapi/Application.java
+++ src/main/java/com/paymentapi/Application.java
@@ -8,5 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+
+
+        // this is a test
     }
 }
